d0b0d2bf8121661598a1f5766fb93d9776f9f2c9
Mike Esposito <mikecancode@gmail.com>
2015-09-27 15:17:51 -0700

    Generate Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   get 'help'    =>  'static_pages#help'
   get 'about'   =>  'static_pages#about'
   get 'contact' =>  'static_pages#contact'
